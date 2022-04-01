538c9d3230a13b9b8a6b657be8bde26aeb32fb62
Telesphoreo <me@telesphoreo.me>
2022-04-01 00:14:49 -0500

    Update example module to make it better suited as a templaet

--- src/main/java/dev/plex/ExampleModule.java
+++ src/main/java/dev/plex/ExampleModule.java
@@ -1,6 +1,7 @@
 package dev.plex;
 
 import dev.plex.command.ExampleCommand;
+import dev.plex.listener.ExampleListener;
 import dev.plex.module.PlexModule;
 
 public class ExampleModule extends PlexModule
@@ -9,10 +10,12 @@ public class ExampleModule extends PlexModule
     public void enable()
     {
         registerCommand(new ExampleCommand());
+        registerListener(new ExampleListener());
     }
 
     @Override
     public void disable()
     {
+        // Unregistering listeners / commands is handled by Plex
     }
 }
